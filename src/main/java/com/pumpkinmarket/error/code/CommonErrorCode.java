488b7537665143170e0b7f9ae576b664fce2d08e
n4oah <n4oahdev@gmail.com>
2024-03-15 17:51:08 +0900

    feat: 비즈니스 로직에서 BusinessException throw 하도록 수정

--- src/main/java/com/pumpkinmarket/error/code/CommonErrorCode.java
+++ src/main/java/com/pumpkinmarket/error/code/CommonErrorCode.java
@@ -10,6 +10,11 @@ public enum CommonErrorCode implements ErrorCode {
             "알 수 없는 에러",
             HttpStatus.INTERNAL_SERVER_ERROR.value()
     ),
+    UNAUTHORIZED_ERROR(
+            "UNAUTHORIZED_ERROR",
+            "인증되지 않은 사용자 입니다.",
+            HttpStatus.UNAUTHORIZED.value()
+    ),
     VALIDATION_ERROR(
             "VALIDATION_ERROR",
             "validation error",
--- src/main/java/com/pumpkinmarket/exception/BusinessException.java
+++ src/main/java/com/pumpkinmarket/exception/BusinessException.java
@@ -7,13 +7,21 @@ import lombok.Getter;
 public class BusinessException extends RuntimeException {
     private final ErrorCode errorCode;
 
-    public BusinessException(final ErrorCode errorCode) {
+    protected BusinessException(final ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 
-    public BusinessException(final ErrorCode errorCode, String exceptionMessage) {
+    protected BusinessException(final ErrorCode errorCode, String exceptionMessage) {
         super(exceptionMessage);
         this.errorCode = errorCode;
+    }
+
+    public static BusinessException of(final ErrorCode errorCode) {
+        return new BusinessException(errorCode);
+    }
+
+    public static BusinessException of(final ErrorCode errorCode, String exceptionMessage) {
+        return new BusinessException(errorCode, exceptionMessage);
     }
 }
